f2da91495b970c2c21b49a65f040507c31578b80
Michio-Kawagoe <201214880+Michio-Kawagoe@users.noreply.github.com>
2025-06-04 08:03:05 +0000

    New Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @Book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to users_path(params[:id])
   end
 
   def index
@@ -9,5 +16,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "homes#top"
+  get 'homes/about' => 'homes#about'
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :edit]
   resources :users, only: [:new, :index, :show, :edit]
