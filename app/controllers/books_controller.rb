4b7dd8b791e666c3775cfc2ebce7a31c6fc5bfd3
Michio-Kawagoe <201214880+Michio-Kawagoe@users.noreply.github.com>
2025-06-10 14:35:52 +0000

    userとbookのshow作り間違えた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,17 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to users_path(params[:id])
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   def index
+    @books = Book.all
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @book = Book.new
   end
 
+
   def index
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.books
+    @books = Book.joins(:user) 
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
